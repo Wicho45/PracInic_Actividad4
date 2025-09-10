c9f7699617a25a7dcd970297354c37474cfc0113
Wicho45 <126707566+Wicho45@users.noreply.github.com>
2025-09-10 00:50:30 -0600

    [ADD]recuperar y registrar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Layout } from './pages/layout/layout';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { Sigin } from './pages/sigin/sigin';
+import { Restore } from './pages/restore/restore';
 
 export const routes: Routes = [
     {
@@ -9,6 +11,12 @@ export const routes: Routes = [
     },
     {
         path: 'login', component:Login
+    },
+    {
+        path: 'sigin', component: Sigin 
+    },
+    {
+        path: 'restore', component: Restore
     },
     {
         path: '', component:Layout, children: [
--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  // loginForm: FormGroup = new FormGroup({
+  //   CorreoId: new FormControl(""),
+  //   Password: new FormControl("")
+  // })
 
+  // http = inject(HttpClient);
+  
+  // onLogin(){
+  //   const formValue = this.loginForm.value;
+  //   this.http.post("", formValue)
+  // }
 }
